34ab494a6ebf0e4d443d2d0ce230ae97a13791ff
Lukáš Gajarský <lukas.gajarsky@gmail.com>
2022-07-17 11:02:04 +0200

    modal-window

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const $buttonOpen = document.querySelector('.mobile-menu__open-button'),
+      $buttonClose = document.querySelector('.menu__close'),
+      $menu = document.querySelector('.menu');
+      
+$buttonOpen.onclick=(e)=>{
+    e.preventDefault()
+    $menu.classList.toggle('active')
+}
+
+$buttonClose.onclick=(e)=>{
+    e.preventDefault()
+    $menu.classList.toggle('active')
+}
