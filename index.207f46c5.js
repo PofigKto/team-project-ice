1622881d529eb5b3d867375a1bc9b854d9277dce
PofigKto <104518418+PofigKto@users.noreply.github.com>
2022-07-17 10:25:43 +0000

    Deploying to gh-pages from @ PofigKto/team-project-ice@9e2a6ccc86ddf820c76393628698b7c4eb1acabb 🚀

---
+++ index.207f46c5.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".mobile-menu__open-button"),t=document.querySelector(".menu__close"),c=document.querySelector(".menu");e.onclick=e=>{e.preventDefault(),c.classList.toggle("active")},t.onclick=e=>{e.preventDefault(),c.classList.toggle("active")};
+//# sourceMappingURL=index.207f46c5.js.map
---
+++ index.e19a7903.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".mobile-menu__open-button"),t=document.querySelector(".menu__close"),c=document.querySelector(".menu");e.onclick=function(e){e.preventDefault(),c.classList.toggle("active")},t.onclick=function(e){e.preventDefault(),c.classList.toggle("active")}}();
+//# sourceMappingURL=index.e19a7903.js.map
