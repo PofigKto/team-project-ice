d808cb261d7b2ed92f3ca03a3ae065bcae2d7e3a
HelenFedoseeva <101935002+HelenFedoseeva@users.noreply.github.com>
2022-07-17 13:08:58 +0000

    Deploying to gh-pages from @ PofigKto/team-project-ice@67db5d3604ec91192735c0381bca6312578db067 🚀

---
+++ index.fc773391.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector(".grid"),e=new Masonry(t,{columnWidth:1,stamp:".stamp",gutter:2});t.addEventListener("click",(function(t){matchesSelector(t.target,".grid-item")&&(t.target.classList.toggle("grid-item--gigante"),e.layout())}))}();
+//# sourceMappingURL=index.fc773391.js.map
