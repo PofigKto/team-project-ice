54f1fbed98dbde0f17e9013069081e5d846191a4
PofigKto <104518418+PofigKto@users.noreply.github.com>
2022-07-18 18:22:43 +0000

    Deploying to gh-pages from @ PofigKto/team-project-ice@7e5634e9b3750dbe366fc7f39d78c77b2f193bd5 🚀

---
+++ index.125bc6f6.js
@@ -0,0 +1,2 @@
+(()=>{const o={openModalBtn:document.querySelector("[data-modal-open-location]"),closeModalBtn:document.querySelector("[data-modal-close-location]"),modal:document.querySelector("[data-modal-location]")};function e(){o.modal.classList.toggle("is-hidden-location")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)})();
+//# sourceMappingURL=index.125bc6f6.js.map
---
+++ index.996d8d3b.js
@@ -0,0 +1,2 @@
+!function(){var e={openModalBtn:document.querySelector("[data-modal-open-franchise]"),closeModalBtn:document.querySelector("[data-modal-close-franchise]"),modal:document.querySelector("[data-modal-franchise]")};function o(){e.modal.classList.toggle("is-hidden-franchise")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
+//# sourceMappingURL=index.996d8d3b.js.map
