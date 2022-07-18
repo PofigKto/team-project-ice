d5389b27e6c99738dcec18f4a867cb2fdc37c4bc
PofigKto <104518418+PofigKto@users.noreply.github.com>
2022-07-18 03:08:05 +0000

    Deploying to gh-pages from @ PofigKto/team-project-ice@bf860a617fff666bcb447f7c3d10bfaebef5d38f 🚀

---
+++ index.0b110751.js
@@ -0,0 +1,2 @@
+new Swiper(".mySwiper",{pagination:{el:".swiper-pagination",clickable:!0,renderBullet:function(n,e){return'<span class="'.concat(e,'"></span>')}}});
+//# sourceMappingURL=index.0b110751.js.map
